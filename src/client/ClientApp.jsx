9f324011b373ec0119ec6c071e35336b2f63f9fd
taekimm <taekimm@gmail.com>
2018-05-30 15:36:37 -0700

    added HMR

--- src/client/ClientApp.jsx
+++ src/client/ClientApp.jsx
@@ -2,4 +2,14 @@ import React from "react";
 import { render } from "react-dom";
 import App from "./components/App";
 
-render(<App />, document.getElementById("root"));
+const renderApp = () => {
+  render(<App />, document.getElementById("root"));
+};
+
+renderApp();
+
+if (module.hot) {
+  module.hot.accept("./components/App", () => {
+    renderApp();
+  });
+}
--- src/server/server.js
+++ src/server/server.js
@@ -3,23 +3,48 @@ const app = express();
 const server = require("http").Server(app);
 const path = require("path");
 
+// webpack stuff for hot reload
+const webpack = require("webpack");
+const webpackConfigURL = path.join(__dirname + "/../../webpack.config")
+const webpackConfig = require(webpackConfigURL);
+const compiler = webpack(webpackConfig);
+
+// wepback HMR
+app.use(
+  require("webpack-dev-middleware")(compiler, {
+    noInfo: true,
+    publicPath: webpackConfig.output.publicPath,
+    stats: false
+  })
+);
+
+app.use(require("webpack-hot-middleware")(compiler));
+
+// static assets
 app.use(express.static(path.join(__dirname, "/../../public")));
 
+// catchall route to send out HTML w/ bundle.js
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname + "/../../public/index.html"));
 });
 
+// setting up port
 const port = process.env.PORT || 5000;
 server.listen(port, () => {
   console.log(`Server listening on ${port}`);
 });
 
+// socket.IO stuff
 const io = require("socket.io")(server);
 
-io.on("connection", client => {
-  io.to(client.id).emit("SOCKET_ID", client.id);
+// vars to hold chatroom data
+var usernames = {};
+var rooms = ['room1', 'room2', 'room3'];
 
-  client.on("SEND_MESSAGE", data => {
-    io.emit("RECEIVE_MESSAGE", data);
-  });
+io.on("connection", socket => {
+  io.to(socket.id).emit("SOCKET_ID", client.id);
+
+  // client.on("SEND_MESSAGE", data => {
+  //   io.emit("RECEIVE_MESSAGE", data);
+  // });
 });
